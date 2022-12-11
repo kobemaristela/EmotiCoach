2b79063d51f4ff26c6fbf1d06e12d4506959d2ba
justinf0428 <justinf0428@gmail.com>
2022-12-11 15:16:18 -0800

    T1007-T1008 Dashboard and Metric Selection Pages Implemented

---
+++ Frontend/Emoticoach/src/app/pages/metric-selection/metric-selection.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-metric-selection',
+  templateUrl: './metric-selection.page.html',
+  styleUrls: ['./metric-selection.page.scss'],
+})
+export class MetricSelectionPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
