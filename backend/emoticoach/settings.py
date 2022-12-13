3d93a2f2447b48ffec49d057ff0d2e1dd5f5737e
Jaemin Lee <35903513+jaemindev@users.noreply.github.com>
2022-12-12 19:18:24 -0800

    Develop t1017 create user login and register api (#12)
    * T1017: User app moved to correct folder. .gitignore updated for mac files. Implemented TokenAuthentication. Created Login, Register, Logout API.
    
    * T1017: Fixed needing a token to register. Deleted duplicate user folder. Modified .gitignore to contain .DS_STORE.
    
    Co-authored-by: Kobe Maristela <kobe@maristela.net>

--- backend/emoticoach/settings.py
+++ backend/emoticoach/settings.py
@@ -39,7 +39,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+
+    "user",
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
