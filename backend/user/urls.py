e855a65766b28be334b77b356fe95ca3fab2a5b2
Kobe Maristela <kobe@maristela.net>
2022-12-12 19:43:45 -0800

    T1018: Updated urls path

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
-from .views import Login, Register, Logout
-from user import views
+from .views import Login, Register, Logout, show_database
 
 urlpatterns = [
     path('login', Login.as_view()),
     path('register', Register.as_view()),
     path('logout', Logout.as_view()),
-    path('showDb', views.show_database)
+    path('showDb', show_database)
 ]
