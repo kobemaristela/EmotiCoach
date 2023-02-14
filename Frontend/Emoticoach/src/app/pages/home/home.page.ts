015d0d8ec914dd17c214680e43acf39a2db3f1a3
Justin Fan <justinfan@Justins-MacBook-Pro.local>
2023-02-14 13:11:09 -0800

    Home page modularization using for loop

--- Frontend/Emoticoach/src/app/pages/home/home.page.ts
+++ Frontend/Emoticoach/src/app/pages/home/home.page.ts
@@ -9,6 +9,38 @@ import { NavController } from '@ionic/angular';
 export class HomePage implements OnInit {
   headers: any;
 
+  homepageCards: string[] = [
+    'Log Workout', 
+    'View RPE', 
+    'Heart Rate',
+    'Sleep and Water',
+    'Tables'
+  ];
+
+  icons: string[] = [
+    'barbell-outline',
+    'walk-outline',
+    'heart-outline',
+    'water-outline',
+    'analytics-outline'
+  ];
+
+  upperText: string[] = [
+    'Log a new workout',
+    'Rate of perceived effort',
+    'Heart Rate',
+    'Sleep and Water',
+    'Tables'
+  ];
+
+  lowerText: string[] = [
+    'Begin a new workout',
+    'View your progress',
+    'View your data',
+    'View your data',
+    'View your data'
+  ];
+
   constructor(public navCtrl: NavController) {
   }
 
@@ -21,5 +53,7 @@ export class HomePage implements OnInit {
   logWorkout(){
     this.navCtrl.navigateForward('/workouts-dashboard')
   }
-
+  createCard(){
+    
+  }
 }
