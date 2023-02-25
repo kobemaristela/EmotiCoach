0b0022addf8f63a11596a3627e1b6d071fb86760
Jaemin Lee <contact.jaemin@gmail.com>
2023-02-25 12:31:13 -0800

    Post request for setting session data, activity data, and set data have been added. requirements.txt updated to include corsheaders. settings.py updated accordingly to apply corsheaders. url and model updated to work with view.

--- backend/workout/views.py
+++ backend/workout/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from .models import Session, Activity
+from .models import Session, Activity, MuscleGroup, Set
 from .controller import parseActivity
+import json
 
 from rest_framework.views import APIView
 from rest_framework.authentication import TokenAuthentication
@@ -31,4 +32,79 @@ class SessionData(APIView):
 
         return JsonResponse(sessionInfo)
     
-    
+class SetSessionData(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        token = request.META['HTTP_AUTHORIZATION'].split()[1]
+
+        sessionObject = request.POST["session"]
+        sessionObject = json.loads(sessionObject)
+        sessionName = sessionObject["name"]
+        sessionDuration = sessionObject["duration"]
+        sessionDatetime = sessionObject["datetime"]
+        userId = Token.objects.get(key=token).user_id
+
+        session = Session.objects.create(name=sessionName,
+                                         duration=sessionDuration, 
+                                         datetime=sessionDatetime,
+                                         auth_user_id=userId)
+        
+        activitiesObject = sessionObject["activities"]
+
+        for activities in activitiesObject:
+            activityName = activities["name"]
+            sessionId = session.id
+
+            activity = Activity.objects.create(name=activityName, 
+                                               session_id=sessionId)
+
+            muscleGroup = activities["muscleGroups"]
+            setsObject = activities["sets"]
+
+            muscle = MuscleGroup.objects.create(activity_id=activity.id)
+            if muscleGroup:
+                for muscles in muscleGroup:
+                    if muscles == "chest":
+                        muscle.update(chest=True)
+                    if muscles == "tricep":
+                        muscle.update(tricep=True)
+                    if muscles == "bicep":
+                        muscle.update(bicep=True)
+                    if muscles == "shoulder":
+                        muscle.update(shoulder=True)
+                    if muscles == "upper_back":
+                        muscle.update(upper_back=True)
+                    if muscles == "lower_back":
+                        muscle.update(lower_back=True)
+                    if muscles == "quadricep":
+                        muscle.update(quadricep=True)
+                    if muscles == "glute":
+                        muscle.update(glute=True)
+                    if muscles == "calve":
+                        muscle.update(calve=True)
+                    if muscles == "abdominal":
+                        muscle.update(abdominal=True)
+                    if muscles == "hamstring":
+                        muscle.update(hamstring=True)
+                
+
+            for sets in setsObject:
+                activitySetNum = sets["set_num"]
+                activityWeight = sets["weight"]
+                activityReps = sets["reps"]
+                activityRpe = sets["rpe"]
+                activityId = activity.id
+
+                set = Set.objects.create(set_num=activitySetNum,
+                                         weight=activityWeight,
+                                         reps=activityReps,
+                                         rpe=activityRpe,
+                                         activity_id=activityId)
+
+
+
+
+        return JsonResponse({"status":"0"})
+        
