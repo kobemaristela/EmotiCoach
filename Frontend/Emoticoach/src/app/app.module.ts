8c6ff800fd93ffc7aabe4b9b61355704b46e81aa
Chad Sherf <chadnsherf@gmail.com>
2023-02-24 17:27:01 -0800

    removed duplicate imports (#53)
    Signed-off-by: csherf <chadnsherf@gmail.com>

--- Frontend/Emoticoach/src/app/app.module.ts
+++ Frontend/Emoticoach/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NgChartsModule } from 'ng2-charts';
-import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [AppComponent],
