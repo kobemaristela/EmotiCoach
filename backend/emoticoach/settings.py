accf3f886be536e1a33e23d8be0034e26880dbb0
Jaemin Lee <contact.jaemin@gmail.com>
2023-02-25 20:08:34 -0800

    Added the paho-mqtt library to Django. Defined mqtt connection variables, and added connection function for mqtt in workout.

--- backend/emoticoach/settings.py
+++ backend/emoticoach/settings.py
@@ -147,3 +147,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# MQTT Settings
+MQTT_SERVER = '10.69.69.182'
+MQTT_PORT = 1883
+MQTT_KEEPALIVE = 60
+MQTT_USER = ''
+MQTT_PASSWORD = ''
