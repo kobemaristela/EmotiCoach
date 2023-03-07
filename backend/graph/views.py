78c7b949a57592fb725fee76be5280f7bdddf080
Jaemin Lee <35903513+jaemindev@users.noreply.github.com>
2023-03-06 22:32:02 -0800

    104 feature muscle group and one rm graph data (#105)
    * Made GetMuscleGroupData api call, returns data from muscle group graph.
    
    * Made api call GetOneRMData which returns graph data for one rm graph.
    
    * Fixed data when more than one session in one day.

--- backend/graph/views.py
+++ backend/graph/views.py
@@ -4,11 +4,13 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.models import Token
 from django.utils.timezone import make_aware
+from django.db.models import Sum
 
 from .mqtt import ppg_infrared, ppg_green, ppg_red
 from django.http import JsonResponse
 from datetime import datetime, timedelta
-from workout.models import Session, Activity, Set
+from workout.models import Session, Activity, Set, MuscleGroup
+from workout.controller import filterMuscleGroups
 
 # Create your views here.
 
@@ -47,8 +49,97 @@ class GetVolumeData(APIView):
             for set in sets:
                 volume += set["weight"] * set["reps"]
             date_key = session["datetime"].strftime("%m/%d")
-            volume_data[date_key] = volume
+
+            if date_key in volume_data:
+                volume_data[date_key] += volume
+            else:
+                volume_data[date_key] = volume
         
         return JsonResponse({"X":list(volume_data.keys()),
                              "y":list(volume_data.values())})
 
+class GetMuscleGroupData(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        token = request.META['HTTP_AUTHORIZATION'].split()[1]
+        userId = Token.objects.get(key=token).user_id
+
+        week_num = int(request.POST["week_num"])
+
+        today = datetime.now()
+        week0 = today - timedelta(days=today.weekday())
+        week_begin = week0 - timedelta(days=week_num * 7)
+        week_begin = make_aware(week_begin)
+        week_end = week_begin + timedelta(days=6)
+
+        sessions = Session.objects.filter(datetime__gte=week_begin, auth_user_id=userId)
+        sessions = sessions.filter(datetime__lte=week_end).values("id", "datetime").order_by("datetime")
+
+        muscleDict = dict()
+        for session in sessions:
+            activity_id = Activity.objects.filter(session_id=session["id"]).values("id")
+            # muscleGroups = MuscleGroup.objects.filter(activity_id__in=activity_id)
+            # reps = Set.objects.filter(activity_id__in=activity_id)
+            muscleList = list()
+            for activity in activity_id:
+                muscleGroups = MuscleGroup.objects.get(activity_id=activity["id"])
+                muscleGroups = filterMuscleGroups(muscleGroups)
+                reps = Set.objects.filter(activity_id=activity["id"]).values("reps")
+                
+                for muscle in muscleGroups:
+                    # print(reps.aggregate(Sum('reps'))['reps__sum'])
+                    repsToAdd = reps.aggregate(Sum('reps'))['reps__sum']
+                    if muscle in muscleDict:
+                        if repsToAdd:
+                            muscleDict[muscle] += repsToAdd
+                    else:
+                        muscleDict[muscle] = repsToAdd
+                
+        return JsonResponse({"X":list(muscleDict.keys()),
+                             "y":list(muscleDict.values())})
+
+
+class GetOneRMData(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        token = request.META['HTTP_AUTHORIZATION'].split()[1]
+        userId = Token.objects.get(key=token).user_id
+
+        start_date = request.POST["start_date"]
+        start_date = make_aware(datetime.strptime(start_date, "%Y-%m-%d"))
+
+        length = int(request.POST["length"])
+        end_date = start_date + timedelta(days=length)
+
+        activity = request.POST["activity"]
+ 
+        sessions = Session.objects.filter(datetime__gte = start_date, auth_user_id=userId)
+        sessions = sessions.filter(datetime__lte = end_date).values("id", "datetime").order_by("datetime")
+
+        one_rm_data = dict()
+        for session in sessions:
+            activities = Activity.objects.filter(session_id=session["id"], name=activity).values("id")
+            sets = Set.objects.filter(activity_id__in=activities).values("weight", "reps")
+            current_one_rm = 0
+            for set in sets:
+                one_rm = set["weight"] / ((1.0278) - (0.0278 * set["reps"]))
+
+                if one_rm > current_one_rm:
+                    current_one_rm = one_rm
+
+            date_key = session["datetime"].strftime("%m/%d")
+
+            one_rm_data[date_key] = current_one_rm
+
+            if date_key in one_rm_data:
+                if one_rm_data[date_key] < current_one_rm:
+                    one_rm_data[date_key] = current_one_rm
+            else:
+                one_rm_data[date_key] = current_one_rm
+
+        return JsonResponse({"X":list(one_rm_data.keys()),
+                             "y":list(one_rm_data.values())})
