213e3939b5f6e100823248f8476df7757493e566
justinf0428 <78179966+justinf0428@users.noreply.github.com>
2023-03-07 15:02:01 -0800

    Graphs connected to backend (#119)
    Co-authored-by: Justin Fan <justinfan@Justins-MacBook-Pro.local>

--- Frontend/Emoticoach/src/app/services/user/request-account.service.ts
+++ Frontend/Emoticoach/src/app/services/user/request-account.service.ts
@@ -6,4 +6,18 @@ import { Injectable } from '@angular/core';
 export class RequestAccountService {
 
   constructor() { }
+
+  async getUserToken(){
+    const formData = new FormData();
+    formData.append("username", "justin");
+    formData.append("password", "fan");
+
+    let tableParam = {
+      method: "POST",
+      body: formData
+  }
+  const res = await fetch("https://emotidev.maristela.net/user/login", tableParam);
+  let registerResponse = await res.json();
+  return registerResponse["token"];
+  }
 }
