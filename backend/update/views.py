dfc8cd20fbb4a02c3c57a17e4ce6d9335c4c5eb7
Kobe Maristela <kobe@maristela.net>
2023-03-16 22:14:51 -0700

    Feature: Server Autoupdate Script (#133)
    * Added update script with webhooks
    
    * Updated code syntax
    
    * Updated file path
    
    * Updated file permissions
    
    * Updated Response Handle
    
    * Reverting changes to emotidev.service
    
    * Updated urls.py

---
+++ backend/update/views.py
@@ -0,0 +1,15 @@
+import subprocess
+import json
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class Update(APIView):
+    def post(self, request):
+        payload = json.loads(request.body)
+        if payload.get('ref') != 'refs/heads/main':
+            return Response('Invalid request', status=status.HTTP_400_BAD_REQUEST)
+            
+        subprocess.Popen(['./update/scripts/update_server.sh'])  # Spawn child process of update_server.sh
+        return Response('Starting Update', status=status.HTTP_200_OK)
