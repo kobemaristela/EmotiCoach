5f94200d5af93206a84f8f57627f609801f84789
justinf0428 <78179966+justinf0428@users.noreply.github.com>
2023-03-27 18:31:03 -0700

    Added get user token function in account service (#152)
    Co-authored-by: Justin Fan <justinfan@justins-mbp.lan>

--- Frontend/Emoticoach/src/app/pages/login/login.page.ts
+++ Frontend/Emoticoach/src/app/pages/login/login.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit,  ViewChild } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { user } from 'src/app/services/user/Iuser';
 import { AccountService } from 'src/app/services/user/account.service';
+import { accountRequest } from 'src/app/services/user/IaccountRequest';
 
 @Component({
   selector: 'app-login',
@@ -16,6 +19,8 @@ export class LoginPage implements OnInit {
     }
 
   user_token = '';
+  user: Observable<any>;
+  user$: Observable<user>;
   isLoggedIn = false;
   
   constructor(private accountService: AccountService, public navCtrl: NavController) { }
@@ -27,6 +32,11 @@ export class LoginPage implements OnInit {
   }
 
   async toHomepage(){
-
+    this.user$ = this.accountService.login(this.userData.username, this.userData.password);
+    this.user$.subscribe((res)=> {
+      if(res){
+        this.navCtrl.navigateForward('/home')
+      }
+    })
   }
 }
