c60408724300d1d9e00f2f4134713f4521d395e3
Jaemin Lee <contact.jaemin@gmail.com>
2023-04-03 21:21:08 -0700

    Login Page finish and base HTML updated accordingly.

--- backend/webapp/urls.py
+++ backend/webapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import Home
+from .views import Home, Login
 
 urlpatterns = [
     path('', Home.as_view(), name="home"),
+    path('login', Login.as_view(), name="login"),
 ]
