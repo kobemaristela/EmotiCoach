93fa243dc7b267825e28141d0e00dfc222383c40
Jaemin Lee <contact.jaemin@gmail.com>
2023-04-04 18:57:43 -0700

    Naming changed in admin for API docs.

--- backend/website/models.py
+++ backend/website/models.py
@@ -5,3 +5,6 @@ class Api(models.Model):
     type = models.CharField(max_length=15)
     address = models.CharField(max_length=100)
     parameters = models.TextField()
+
+    def __str__(self):
+        return self.address
