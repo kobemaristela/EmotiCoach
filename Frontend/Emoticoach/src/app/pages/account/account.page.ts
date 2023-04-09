2de289899dd4446f6f7d72988c12b2e9dd31b36a
justinf0428 <justinf0428@gmail.com>
2023-04-09 14:32:51 -0700

    Account page complete, still need to add change password and server cert.

--- Frontend/Emoticoach/src/app/pages/account/account.page.ts
+++ Frontend/Emoticoach/src/app/pages/account/account.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from 'src/app/services/user/account.service';
 
 @Component({
   selector: 'app-account',
@@ -7,9 +8,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountPage implements OnInit {
 
-  constructor() { }
+  user_email: string;
+  user_firstlast: string;
+  accountPage: boolean;
+  emotibitPage: boolean;
+  settingsPage: boolean;
+  accountButton: string; //outline: not selected, solid: selected
+  emotibitButton: string;
+  settingsButton: string;
+  timezone: string;
+
+  constructor(private accountService: AccountService) {
+    this.accountPage = true;
+    this.emotibitPage = false;
+    this.settingsPage = false;
+    this.accountButton = "solid"
+    this.emotibitButton = "outline"
+    this.settingsButton = "outline"
+   }
+
+  displayEmail(){
+    this.user_email = this.accountService.returnUserEmail();
+  }
+  displayName(){
+    this.user_firstlast = this.accountService.returnFirstLastName();
+  }
+
+  displayAccountPage(){
+    this.accountButton = "solid"
+    this.emotibitButton = "outline"
+    this.settingsButton = "outline"
+    this.accountPage = true;
+    this.emotibitPage = false;
+    this.settingsPage = false;
+  }
+  displayEmotibitPage(){
+    this.accountButton = "outline"
+    this.emotibitButton = "solid"
+    this.settingsButton = "outline"
+    this.accountPage = false;
+    this.emotibitPage = true;
+    this.settingsPage = false;
+  }
+
+  displaySettingsPage(){
+    this.accountButton = "outline"
+    this.emotibitButton = "outline"
+    this.settingsButton = "solid"
+    this.accountPage = false;
+    this.emotibitPage = false;
+    this.settingsPage = true;
+  }
+
+  displayTimezone(){
+    this.timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
+    return this.timezone;
+  }
 
   ngOnInit() {
+    this.displayEmail()
+    this.displayName()
+    this.displayTimezone()
   }
 
 }
