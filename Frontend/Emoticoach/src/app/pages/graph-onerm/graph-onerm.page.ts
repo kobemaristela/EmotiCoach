552f9601c8037fa4d9e42214823ed4c23cb98ee2
justinf0428 <78179966+justinf0428@users.noreply.github.com>
2023-04-09 15:02:36 -0700

    Graphs complete (#195)
    * Graphs complete
    
    * Muscle Chart bug fixes

--- Frontend/Emoticoach/src/app/pages/graph-onerm/graph-onerm.page.ts
+++ Frontend/Emoticoach/src/app/pages/graph-onerm/graph-onerm.page.ts
@@ -13,8 +13,30 @@ export class GraphOnermPage implements OnInit {
   workouts: any[] = [];
   workoutDates: string[] = [];
   selectedWorkout = "";
+  xAxisDates: string[] = [];
+  previousWeek: Date;
+  previousWeekFormatted: string;
+  rightsideWeek: Date;
+  rightsideWeekFormatted: string;
 
-  constructor(private graphService: GraphService) { }
+  constructor(private graphService: GraphService) {
+    this.previousWeek = this.graphService.getPreviousWeek(this.graphService.getCurrentDate());
+    this.rightsideWeek = this.graphService.getCurrentDate();
+    this.previousWeekFormatted = this.graphService.formatDate(this.graphService.getPreviousWeek(this.graphService.currentDate))
+    this.rightsideWeekFormatted = this.graphService.formatDate(this.graphService.getCurrentDate())
+   }
+
+   returnPast7Days(date: Date) {
+    return Array(7).fill(new Date(date)).map((el, idx) =>
+      new Date(el.setDate(el.getDate() - el.getDay() + idx)))
+  }
+
+    formatXaxis(dates: Date[]){
+    this.xAxisDates.length = 0
+    for(let i=0; i<dates.length; i++){
+      this.xAxisDates.push(this.graphService.formatDate(dates[i]));
+    }
+  }
 
   getWorkoutNames(){
     this.graphService.getActivityNames().subscribe(data => {
@@ -24,13 +46,33 @@ export class GraphOnermPage implements OnInit {
     })
   }
 
+  displayPreviousWeek(){
+    this.formatXaxis(this.returnPast7Days(this.previousWeek))
+    this.previousWeek = this.graphService.getPreviousWeek(this.previousWeek);
+    this.rightsideWeek = this.graphService.getPreviousWeek(this.rightsideWeek);
+    this.previousWeekFormatted = this.xAxisDates[0];
+    this.rightsideWeekFormatted = this.xAxisDates[6];
+    this.updateChart();
+  }
+
+  displayNextWeek(){
+    this.formatXaxis(this.returnPast7Days(this.graphService.getNextWeek(this.rightsideWeek)))
+    this.previousWeek = this.graphService.getNextWeek(this.previousWeek);
+    this.rightsideWeek = this.graphService.getNextWeek(this.rightsideWeek);
+    this.previousWeekFormatted = this.xAxisDates[0];
+    this.rightsideWeekFormatted = this.xAxisDates[6];
+    this.updateChart();
+  }
+
   updateChart(){
-    this.graphService.getOneRMXandY("2023-03-23", this.selectedWorkout).subscribe( x_data => {
+    this.graphService.getOneRMXandY(this.graphService.formatDateforAPI(this.previousWeek), this.selectedWorkout).subscribe( x_data => {
+      this.workoutDates.length = 7;
       this.workoutDates = x_data.X;
-      this.chart.data.labels = this.workoutDates;
+      this.chart.data.labels = this.xAxisDates;
       this.chart.update();
     });
-    this.graphService.getOneRMXandY("2023-03-23", this.selectedWorkout).subscribe( y_data => {
+    this.graphService.getOneRMXandY(this.graphService.formatDateforAPI(this.previousWeek), this.selectedWorkout).subscribe( y_data => {
+      this.workoutData.length = 7;
       this.workoutData = y_data.y;
       this.chart.data.datasets[0].data = this.workoutData;
       this.chart.update();
@@ -53,8 +95,21 @@ export class GraphOnermPage implements OnInit {
         ]
       },
       options: {
-        aspectRatio: 1
-          }
+        aspectRatio: 1,
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+        scales: {
+          x: {
+            ticks: {
+              display: true,
+              autoSkip: false,
+            },
+          },
+        },
+      },
     });
 
     this.chart.options.animation = true;
