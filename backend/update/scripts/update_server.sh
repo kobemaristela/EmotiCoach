7f0a7577cacfb4d1de4de375d0ccdd4f8f59a115
Kobe Maristela <kobe@maristela.net>
2023-04-14 14:59:42 -0700

    Update update_server.sh

--- backend/update/scripts/update_server.sh
+++ backend/update/scripts/update_server.sh
@@ -2,17 +2,19 @@
 
 # Go to server directory
 # SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd ../..
+# cd ../..
+
+echo $PWD
 
 # Update changes from main
-git pull
+#git pull
 
 # Install new Python dependencies
-pip3 install -r requirements.txt
+#pip3 install -r requirements.txt
 
 # Run Django database migrations
-python3 manage.py makemigrations
-python3 manage.py migrate
+#python3 manage.py makemigrations
+#python3 manage.py migrate
 
 # Restart service
-systemctl restart emotidev.service  # Development Service
+#systemctl restart emotidev.service  # Development Service
