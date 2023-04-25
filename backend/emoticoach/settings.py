9666cddb8a889fa781f5b7461646b68a3b9e35d0
Kobe Maristela <kobe@maristela.net>
2023-04-24 23:38:27 -0700

    Updated brogrammers website

--- backend/emoticoach/settings.py
+++ backend/emoticoach/settings.py
@@ -82,7 +82,7 @@ ROOT_URLCONF = 'emoticoach.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'website/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -148,6 +148,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'website/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
